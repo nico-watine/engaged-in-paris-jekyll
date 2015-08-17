e928491c9489d6d8c4c0706754e08b9816873be3
Nico Watine <contact@studioncreations.com>
2015-08-17 10:09:50 -0500

    Index response @400px fixed
    Specifically the nav li widths

--- wedding-details.php
+++ wedding-details.php
@@ -8,7 +8,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
-	<title>Nico &amp; Corinne Watine are Engaged!</title>
+	<title>Nico &amp; Corinne are Engaged!</title>
 	<meta name="description" content="###">
 	<link type="text/plain" rel="author" href="/humans.txt">
 	
