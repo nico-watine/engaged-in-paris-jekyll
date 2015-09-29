d95ab9dfb63e48d8c330e17df0c1a731713a4857
Nico Watine <contact@studioncreations.com>
2015-09-29 16:45:55 -0500

    Pre-merge github note

--- proposal-story.php
+++ proposal-story.php
@@ -150,6 +150,8 @@
 
 </div><!-- /#skrollr-body -->
 
+<!-- MINOR NOTE FOR GITHUB EDIT -->
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/vendor/jquery/1-11-3/jquery-min.js"><\/script>')</script>
 <script src="<?php autoVer('/js/scripts-min.js'); ?>"></script>
