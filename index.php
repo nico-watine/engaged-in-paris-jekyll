4f57104ee581e948002ffe51e46ca91a8d273c0f
Nico Watine <contact@studioncreations.com>
2015-10-23 10:39:00 -0500

    Minor edits to <head>s on all pages
    Removed all un-necessary support for IE8

--- index.php
+++ index.php
@@ -5,23 +5,15 @@
 <html class="no-js" lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
 	<title>Nico &amp; Corinne are Engaged!</title>
 	<meta name="description" content="###">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link type="text/plain" rel="author" href="/humans.txt">
 	
 	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>"/>
-
-	<!--[if lt IE 9]>
-		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>"/>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<![endif]-->
-
 </head>
 <body id="index">
-
 
 <img class="wedding-crest" src="/img/badge/badge.png" alt="Nico &amp; Corinne Wedding Crest">
 
