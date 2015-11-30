5807cb865565dcd43d22d07dc2b1fd98adae42a0
Nico Watine <contact@studioncreations.com>
2015-11-30 10:59:03 -0600

    Work on RSVP

--- _site/rsvp.php
+++ _site/rsvp.php
@@ -61,7 +61,33 @@
 	<section class="content-container centered-text tab-content" id="main-wrapper">
 		<article id="rsvp" class="tab-pane fade in active">
 			<h2 class="header">RSVP</h2>
-			<p class="large">The favor of your reply is requested by January 20, 2016.</p>
+			<p class="large">The favor of your reply is requested by <span class="inline-block">January 20, 2016.</span></p>
+				<form name="rsvp-form" id="rsvp-form" action="/php/submit-rsvp" enctype="multipart/form-data" method="post" /> 
+				    <div class="row">
+				        <div class="form-row">
+			                Your full name<input type="text" id="name" name="name" placeholder="your name" />
+				        </div>
+				        <div class="form-row">
+			                Your email<input type="text" id="email" name="email" placeholder="your email address" />
+				        </div>
+				        <div class="form-row" id="buttons">
+				            <div class="form-button">
+				                <input type="radio" name="site-status" value="I already have a website"><label for="have-site">I already have a website</label>
+				            </div>
+				            <div class="form-button" id="omega">
+				                <input type="radio" name="site-status" value="I don't have my own site"><label for="no-site">I don't have my own site</label>
+				            </div>
+				        </div>
+				        <div class="form-row" id="message-box">
+				            <label>So how can I help?
+				                <textarea type="text" id="msg" name="message" placeholder="your message"></textarea>
+				            </label>
+				        </div>
+				        <div class="form-row">
+				            <button type="submit" name="submit" id="submit" class="button big-green-button header">Send away...</button>
+				        </div>
+				    </div>
+				</form>
 		</article>
 	</section>
 </main>
