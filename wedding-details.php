42a5720671c8a3d00ca57bbf5b7c76aab5065be0
Nico Watine <contact@studioncreations.com>
2016-01-03 14:02:05 -0600

    wedding-details.php DONE!!!

--- wedding-details.php
+++ wedding-details.php
@@ -1,5 +1,5 @@
 ---
-layout: wedding-details/registry
+layout: wedding-details/ceremony-reception-rsvp-registry
 title: Wedding Details
 description: All info regarding the mass and reception can be found on this page.
 ---
