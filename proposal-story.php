939bf558d3275257a3abdcc9b0edc647cbfeb8b8
Nico Watine <contact@studioncreations.com>
2016-01-04 13:43:51 -0600

    New full 4-tabbed wedding-details page is online

--- proposal-story.php
+++ proposal-story.php
@@ -1,5 +1,5 @@
 ---
-layout: proposal-story/final-proposal
+layout: proposal-story/3-opportunities
 title: Awesome Proposal Story
 description: This is the amazing proposal story of Nico &amp; Corinne's engagement in Paris.
 ---
