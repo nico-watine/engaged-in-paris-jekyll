cd45744d58f373ac130076c6a77821d4ef926f07
Nico Watine <contact@studioncreations.com>
2016-01-09 13:42:12 -0600

    Deleted unused scripts.php
    This has been duplicated to jquery.php

--- _includes/components/scripts/scripts.php
+++
@@ -1,2 +0,0 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="/js/vendor/jquery/1-11-3/jquery-min.js"><\/script>')</script>
