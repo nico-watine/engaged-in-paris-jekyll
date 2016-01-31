4fabcf39369f789d073fdf0c3e412b62c0e165b3
Nico Watine <contact@studioncreations.com>
2016-01-31 14:13:18 -0600

    New reception only layout created to edit map link
    Edit DONE - merge into master layout

--- wedding-details.php
+++ wedding-details.php
@@ -1,6 +1,6 @@
 ---
-layout: wedding-details/ceremony-reception-rsvp-registry-thanks-modal
-<!-- layout: wedding-details/ceremony -->
+<!-- layout: wedding-details/ceremony-reception-rsvp-registry-thanks-modal -->
+layout: wedding-details/reception
 title: Wedding Details
 description: All info regarding the mass and reception can be found on this page.
 ---
