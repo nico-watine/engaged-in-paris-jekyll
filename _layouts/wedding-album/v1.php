e499403d33d02edc0059836f207e8e44ffab4c95
Nico Watine <nico@studioncreations.com>
2016-03-01 11:40:31 -0600

    Wedding-Album <header> & beginning <main> with #first-dance

--- _layouts/wedding-album/v1.php
+++ _layouts/wedding-album/v1.php
@@ -1,27 +1,45 @@
 {% include components/head.php %}
 
 <body id="wedding-album">
-<nav>
-	<div class="outer-container">
-		<div class="row">
-			<div id="crest-wrapper">
-				<a href="/">
-					<img src="{{ site.cdn_url }}/img/badge/nav-badge.png" alt="Mini Crest">
-				</a>
+
+<header>
+	<nav>
+		<div class="outer-container">
+			<div class="row">
+				<div id="crest-wrapper">
+					<a href="/">
+						<img src="{{ site.cdn_url }}/img/badge/nav-badge.png" alt="Mini Crest">
+					</a>
+				</div>
+				<ul>
+					<li id="wedding-details">
+						<a href="/wedding-album" class="active">Wedding Album</a>
+					</li>
+					<li id="proposal-story">
+						<a href="/proposal-story">Proposal Story</a>
+					</li>
+				</ul>
 			</div>
-			<ul>
-				<li id="wedding-details">
-					<a href="/wedding-album" class="active">Wedding Album</a>
-				</li>
-				<li id="proposal-story">
-					<a href="/proposal-story">Proposal Story</a>
-				</li>
-			</ul>
 		</div>
-	</div>
-</nav>
+	</nav>
+	<h1 class="title white text-shadow">Wedding Album</h1>
+</header>
 
-
+<main>
+	<section id="memories">
+		<h2 class="header centered-text">The wedding may be over, <br>
+		but the memories will last forever</h2>
+	</section>
+	<section id="first-dance">
+		<div class="outer-container">
+			<div class="row">
+				<div class="abstract-swirl">
+					<img srcset="{{ site.cdn_url }}/img/wedding-album/first-dance/first-dance-swirl@2x.png 2x" src="{{ site.cdn_url }}/img/wedding-album/first-dance/first-dance-swirl.png" alt="Abstract Swirl">
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
 
 
 <!-- {% include components/scripts/jquery.php %} -->
--- _site/wedding-album.php
+++ _site/wedding-album.php
@@ -16,27 +16,45 @@
 </head>
 
 <body id="wedding-album">
-<nav>
-	<div class="outer-container">
-		<div class="row">
-			<div id="crest-wrapper">
-				<a href="/">
-					<img src="/img/badge/nav-badge.png" alt="Mini Crest">
-				</a>
+
+<header>
+	<nav>
+		<div class="outer-container">
+			<div class="row">
+				<div id="crest-wrapper">
+					<a href="/">
+						<img src="/img/badge/nav-badge.png" alt="Mini Crest">
+					</a>
+				</div>
+				<ul>
+					<li id="wedding-details">
+						<a href="/wedding-album" class="active">Wedding Album</a>
+					</li>
+					<li id="proposal-story">
+						<a href="/proposal-story">Proposal Story</a>
+					</li>
+				</ul>
 			</div>
-			<ul>
-				<li id="wedding-details">
-					<a href="/wedding-album" class="active">Wedding Album</a>
-				</li>
-				<li id="proposal-story">
-					<a href="/proposal-story">Proposal Story</a>
-				</li>
-			</ul>
 		</div>
-	</div>
-</nav>
+	</nav>
+	<h1 class="title white text-shadow">Wedding Album</h1>
+</header>
 
-
+<main>
+	<section id="memories">
+		<h2 class="header centered-text">The wedding may be over, <br>
+		but the memories will last forever</h2>
+	</section>
+	<section id="first-dance">
+		<div class="outer-container">
+			<div class="row">
+				<div class="abstract-swirl">
+					<img srcset="/img/wedding-album/first-dance/first-dance-swirl@2x.png 2x" src="/img/wedding-album/first-dance/first-dance-swirl.png" alt="Abstract Swirl">
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
 
 
 <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
