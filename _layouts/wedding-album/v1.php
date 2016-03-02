345f8a2243d808ce2825ea7894cfc62386824b0c
Nico Watine <nico@studioncreations.com>
2016-03-02 11:32:03 -0600

    #hair-makup DONE @desktop

--- _layouts/wedding-album/v1.php
+++ _layouts/wedding-album/v1.php
@@ -30,13 +30,73 @@
 		<h2 class="header centered-text">The wedding may be over, <br>
 		but the memories will last forever</h2>
 	</section>
-	<section id="first-dance" style="margin-bottom: 200px;">
+	<section id="first-dance">
 		<div class="outer-container">
 			<div class="row">
 				<h2 class="title white text-shadow centered-text">First Dance Video</h2>
 				<div class="video-wrapper">
 					<div class="iframe-wrapper">
 						<iframe src="https://www.youtube.com/embed/EnB86TDmzvY" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="hair-makeup">
+		<div class="outer-container">
+			<div class="row centered-text">
+				<h2 class="title">Ladies' Hair &amp; Makeup Prep</h2>
+				<h3>Getting ready at Hotel Zaza</h3>
+			</div>
+			<div class="row double padding">
+				<div class="portrait">
+					<div class="photo-wrapper">
+						<img srcset="/img/wedding-album/hair-makeup/2@2x.jpg 2x" src="/img/wedding-album/hair-makeup/2.jpg" alt="Shayne, Gigi &amp; Lola">
+					</div>
+				</div>
+				<div class="landscape">
+					<img srcset="/img/wedding-album/hair-makeup/10@2x.jpg 2x" src="/img/wedding-album/hair-makeup/10.jpg" alt="Shayne, Gigi &amp; Lola">
+				</div>
+			</div>
+			<div class="row triple padding">
+				<div class="first photo">
+					<img srcset="/img/wedding-album/hair-makeup/2@2x.jpg 2x" src="/img/wedding-album/hair-makeup/2.jpg" alt="Shayne, Gigi &amp; Lola">
+				</div>
+				<div class="photo">
+					<img srcset="/img/wedding-album/hair-makeup/3@2x.jpg 2x" src="/img/wedding-album/hair-makeup/3.jpg" alt="Shayne, Gigi &amp; Lola">
+				</div>
+				<div class="photo">
+					<img srcset="/img/wedding-album/hair-makeup/4@2x.jpg 2x" src="/img/wedding-album/hair-makeup/4.jpg" alt="Shayne, Gigi &amp; Lola">
+				</div>
+			</div>
+			<div class="row triple padding">
+				<div class="first photo">
+					<img srcset="/img/wedding-album/hair-makeup/5@2x.jpg 2x" src="/img/wedding-album/hair-makeup/5.jpg" alt="Shayne, Gigi &amp; Lola">
+				</div>
+				<div class="photo">
+					<img srcset="/img/wedding-album/hair-makeup/6@2x.jpg 2x" src="/img/wedding-album/hair-makeup/6.jpg" alt="Shayne, Gigi &amp; Lola">
+				</div>
+				<div class="photo">
+					<img srcset="/img/wedding-album/hair-makeup/7@2x.jpg 2x" src="/img/wedding-album/hair-makeup/7.jpg" alt="Shayne, Gigi &amp; Lola">
+				</div>
+			</div>
+			<div class="row double padding">
+				<div class="portrait">
+					<div class="photo-wrapper">
+						<img srcset="/img/wedding-album/hair-makeup/8@2x.jpg 2x" src="/img/wedding-album/hair-makeup/8.jpg" alt="Shayne, Gigi &amp; Lola">
+					</div>
+				</div>
+				<div class="landscape">
+					<img srcset="/img/wedding-album/hair-makeup/11@2x.jpg 2x" src="/img/wedding-album/hair-makeup/11.jpg" alt="Shayne, Gigi &amp; Lola">
+				</div>
+			</div>
+			<div class="row double padding">
+				<div class="first landscape">
+					<img srcset="/img/wedding-album/hair-makeup/11@2x.jpg 2x" src="/img/wedding-album/hair-makeup/11.jpg" alt="Shayne, Gigi &amp; Lola">
+				</div>
+				<div class="portrait">
+					<div class="photo-wrapper">
+						<img srcset="/img/wedding-album/hair-makeup/8@2x.jpg 2x" src="/img/wedding-album/hair-makeup/8.jpg" alt="Shayne, Gigi &amp; Lola">
 					</div>
 				</div>
 			</div>
@@ -49,4 +109,3 @@
 {% include components/scripts/google-tracking.php %}
 </body>
 
-<!-- width="745" height="420"  -->
